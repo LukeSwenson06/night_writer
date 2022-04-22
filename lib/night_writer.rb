9d022d684b318c843eefe58a356d70aabfd27200
Luke Swenson <LukeSwenson95@outlook.com>
2022-04-22 10:22:30 -0600

    feat: working file reader and file writer

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,1 +1,11 @@
 
+file_reader = File.open(ARGV[0], "r")
+message_reader = file_reader.read
+file_reader.close
+
+puts message_reader
+capitalized = message_reader.upcase
+
+writer = File.open(ARGV[1], "w")
+writer.write(capitalized)
+writer.close
